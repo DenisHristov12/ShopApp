0b8976b2d77fcb4f198d1ceca95f25229e0648df
Denis Hristov <48285397+DenisHristov12@users.noreply.github.com>
2025-02-04 00:21:27 +0200

    add spacing and typography system

--- UniversalShop/src/styles/GlobalStyles.js
+++ UniversalShop/src/styles/GlobalStyles.js
@@ -3,7 +3,29 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
 :root {
     
-    /* Define typography and spacing systems */
+   /* typography system */
+   --text-s-1: 0.4rem;
+    --text-s-2:   0.6rem;
+    --text-s-3:   0.8rem;
+    --text-md-1:   1.4rem;
+    --text-md-2:   1.6rem;
+    --text-md-3:   2rem;
+    --text-lg-1:  2.4rem;
+    --text-lg-2: 2.8rem;
+    --text-lg-3:   3.2rem;
+    --text-xl:   4rem;
+    --text-xxl:  4.8rem;
+    --text-xxxl:  6.2rem;
+
+
+    /* spacing system */
+    --space-xxs:  0.4rem;
+    --space-xs:   0.8rem;
+    --space-sm:   1.2rem;
+    --space-md:   2rem;
+    --space-lg:   3.2rem;
+    --space-xl:   5.2rem;
+    --space-xxl:  8.4rem;
     
     *,
     *::before,
@@ -18,7 +40,8 @@ const GlobalStyles = createGlobalStyle`
     }
 
     body {
-        font-family: 'Courier New', Courier, monospace; //placeholder font family
+        font-family: 'Roboto', serif;
+        font-weight: 500;
         color: black; //placeholder color for text
         min-height: 100vh;
         line-height: 1.5;
